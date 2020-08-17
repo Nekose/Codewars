945b92bb79cb5e5a5a27a608c197deaccd2bc84d
Peter Rucker <peter@immunoconcepts.com>
2020-08-17 08:49:40 -0700

    Scrambles passes test cases

--- src/Scrambles.py
+++ src/Scrambles.py
@@ -8,22 +8,16 @@ def scramble(s1: str, s2: str) -> bool:
     If s2 can be found within s1, return True. Else return False
     """
     scrambledict = {}
-    worddict = {}
     for letter in s1:
         if letter in scrambledict:
             scrambledict[letter] += 1
         else:
             scrambledict[letter] = 1
-    for letter in s2:
-        if letter in worddict:
-            worddict[letter] += 1
-        else:
-            worddict[letter] = 1
     try:
-        for element in worddict:
-            lettercount = worddict[element]
-            if lettercount > scrambledict[element]:
+        for letter in s2:
+            scrambledict[letter] -= 1
+            if scrambledict[letter] < 0:
                 return False
-        return True
     except KeyError:
         return False
+    return True
